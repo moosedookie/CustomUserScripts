877326c6104426fb4fd2fc43f837b9e82c19d041
moosedookie <moosedookie84@gmail.com>
2022-12-28 03:58:35 -0600

    forgot to include site...

--- ForceSSLTorrentInvites/Force-SSL-Torrent-Invites.user.js
+++ ForceSSLTorrentInvites/Force-SSL-Torrent-Invites.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name        Force SSL Torrent Invites
 // @description force HTTPS on torrentinvites.org
-// @version     1.0
+// @version     1.1
 // @author      2022 moosedookie
 // @icon        https://moosedookie.github.io/CustomUserScripts/ForceSSLTorrentInvites/icon.png
 // @homepage    https://github.com/moosedookie/CustomUserScripts
 // @namespace   https://moosedookie.github.io/CustomUserScripts/ForceSSLTorrentInvites
 // @grant       unsafeWindow
 // @run-at      document-start
+// @match       http://torrentinvites.org/*
 // @updateURL   https://moosedookie.github.io/CustomUserScripts/ForceSSLTorrentInvites/Force-SSL-Torrent-Invites.user.js
 // @downloadURL https://moosedookie.github.io/CustomUserScripts/ForceSSLTorrentInvites/Force-SSL-Torrent-Invites.user.js
 // ==/UserScript==
